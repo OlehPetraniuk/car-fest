f730663f99a06bddcf2a24e2fa949d55f8f5f19f
Oleh Petraniuk <olehshort@gmail.com>
2018-12-19 17:46:10 +0100

    Added component SPA

---
+++ src/spa/spa.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { SpaBodyComponent } from './spa-body/spa-body.component';
+
+@NgModule ({
+    imports: [CommonModule],
+    declarations: [SpaBodyComponent],
+    exports: [SpaBodyComponent]
+})
+export class SpaModule {}
