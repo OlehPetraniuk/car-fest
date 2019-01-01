e38aed9954d6e4092e28711d080c6248078d9090
Oleh Petraniuk <olehshort@gmail.com>
2019-01-01 22:31:51 +0100

    Modified menu display

--- src/spa/services/menu.service.ts
+++ src/spa/services/menu.service.ts
@@ -15,4 +15,8 @@ export class MenuService {
         this.isVertical = true;
         this.showVerticalMenu = !this.showVerticalMenu;
     }
+
+    toggleOrientation(): void {
+        this.isVertical = !this.isVertical;
+    }
 }
