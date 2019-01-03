453ba8176d5719dccc56c75cbd738042ee30c72a
Oleh Petraniuk <olehshort@gmail.com>
2019-01-03 17:07:37 +0100

    Added new functionality of popup menu

--- src/spa/menus/menu-item/menu-item.component.ts
+++ src/spa/menus/menu-item/menu-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer, ElementRef, HostListener, HostBinding} from '@angular/core';
 import { MenuItem, MenuService } from 'src/spa/services/menu.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'spa-menu-item',
@@ -8,9 +9,58 @@ import { MenuItem, MenuService } from 'src/spa/services/menu.service';
 })
 export class MenuItemComponent implements OnInit {
   @Input() item: MenuItem;
-  constructor(private menuService: MenuService) { }
+  @HostBinding('class.parent-for-popup')
+  @Input() parentIsPopup = true;
+  mouseInPopup = false;
+  mouseInItem = false;
+  popupLeft = 0;
+  popupTop = 42;
+  isActiveRoute = false;
+  constructor(private menuService: MenuService,
+    private router: Router,
+    private el: ElementRef,
+    private renderer:Renderer) { }
 
   ngOnInit() {
+    this.checkingActiveRoute(this.router.url);
+    this.router.events.subscribe((event) => {
+      if( event instanceof NavigationEnd) {
+        this.checkingActiveRoute(event.url);
+      }
+    });
   }
 
+  checkingActiveRoute(route: string): void {
+    this.isActiveRoute = (route === this.item.route);
+  }
+
+  onPopupMouseLeave(event: Event): void {
+    if (!this.menuService.isVertical) {
+      this.mouseInPopup = false;  }
+  }
+  onPopupMouseEnter(event: Event): void {
+    if (!this.menuService.isVertical) {
+      this.mouseInPopup = true;
+    }
+  }
+
+@HostListener('mouseleave', ['$event'])
+onMouseLeave(event): void {
+  if (!this.menuService.isVertical) {
+    this.mouseInItem = false;
+  }
+}
+
+@HostListener('mouseenter')
+onMouseEnter(): void {
+ if (!this.menuService.isVertical) {
+   if(this.item.submenu) {
+     this.mouseInItem =true;
+     if (this.parentIsPopup) {
+       this.popupLeft = 160;
+       this.popupTop = 0;
+     }
+   }
+  }
+}
 }
