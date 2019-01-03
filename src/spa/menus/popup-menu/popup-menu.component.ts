d03b3c045cea551e817933ce8d5717e2e7057e24
Oleh Petraniuk <olehshort@gmail.com>
2019-01-03 16:34:35 +0100

    Created functionality of popup menu

--- src/spa/menus/popup-menu/popup-menu.component.ts
+++ src/spa/menus/popup-menu/popup-menu.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MenuService, MenuItem } from '../../services/menu.service';
 
 @Component({
-  selector: 'app-popup-menu',
+  selector: 'spa-popup-menu',
   templateUrl: './popup-menu.component.html',
   styleUrls: ['./popup-menu.component.css']
 })
 export class PopupMenuComponent implements OnInit {
+  @Input() menu: Array<MenuItem>
 
   constructor() { }
 
