f1afb0094f92b7e74a98c530e3f184d63510ddf7
Oleh Petraniuk <olehshort@gmail.com>
2019-01-05 23:28:39 +0100

    Init Authenticate component

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -4,12 +4,15 @@ import { SettingsComponent } from './settings/settings.component';
 import { CarMaintComponent } from './car-maint/car-maint.component';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarDetailComponent } from './car-detail/car-detail.component';
+import { AuthenticatedComponent } from './authenticated/authenticated.component';
 export const appRoutes: Routes = [
-    {path: 'home', component: AppHomeComponent},
-    {path: 'settings', component: SettingsComponent},
-    {path: 'car-maint', component: CarMaintComponent},
-    {path: 'car-list/:count', component: CarListComponent },
-    {path: 'car-detail/:car', component: CarDetailComponent},
+    {path: 'authenticated', component: AuthenticatedComponent, children: [
+        {path: 'home', component: AppHomeComponent},
+        {path: 'settings', component: SettingsComponent},
+        {path: 'car-maint', component: CarMaintComponent},
+        {path: 'car-list/:count', component: CarListComponent },
+        {path: 'car-detail/:car', component: CarDetailComponent},
+    ]},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: '**', component: AppHomeComponent},
 
