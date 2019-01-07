a56418e62d262515a99dafe08b5cace5d07b58ce
Oleh Petraniuk <olehshort@gmail.com>
2019-01-08 00:34:36 +0100

    Added functionality to sign-in form

--- src/spa/users/sign-in/sign-in.component.ts
+++ src/spa/users/sign-in/sign-in.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserApi } from '../user-api';
+import { UserService } from '../../../app/services/user.service';
+import { Router } from '@angular/router';
+import { NgForm } from '../../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'spa-sign-in',
@@ -9,8 +12,21 @@ import { UserApi } from '../user-api';
 export class SignInComponent implements OnInit {
   submitting = false;
   formError: string;
-  constructor(private userApi: UserApi) { }
-
+  constructor(private userApi: UserApi, private userService:UserService, private router: Router) { }
+  onSubmit(signInForm: NgForm): void {
+    if (signInForm.valid) {
+      this.submitting = true;
+      this.formError = null;
+      this.userApi.signIn(signInForm.value.email, signInForm.value.password).subscribe((data) => {
+        console.log(data);
+        this.router.navigate(['/authenticated']);
+      },
+    (error) => {
+      this.submitting = false;
+      this.formError = error;
+    });
+    }
+  }
   ngOnInit() {
   }
 
