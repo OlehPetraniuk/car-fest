d37851a390f106f9f0e0b7e1af4c67e4b723eeac
Oleh Petraniuk <olehshort@gmail.com>
2019-01-09 01:19:18 +0100

    Added logical functionality to auth-guardm

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,3 +1,15 @@
 import { Injectable } from '@angular/core';
-Injectable()
-export class AuthGuard {}
+import {CanActivate, Router} from '@angular/router';
+import {UserService} from './user.service';
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private router: Router, private userService: UserService) {
+
+    }
+    canActivate():boolean {
+        if (!this.userService.isAuthenticated) {
+this.router.navigate(['/sign-in']);
+        }
+        return this.userService.isAuthenticated;
+    }
+}
