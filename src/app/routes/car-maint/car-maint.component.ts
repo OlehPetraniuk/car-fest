cd47174101c85bd8e1b0e5776a2ae3843a9272b9
Oleh Petraniuk <olehshort@gmail.com>
2019-01-10 09:00:20 +0100

    Added logical functionality to app-data & CarList to Maint component

--- src/app/routes/car-maint/car-maint.component.ts
+++ src/app/routes/car-maint/car-maint.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from '../../services/car-interface';
+import { Router } from '@angular/router';
+import { AppDataService } from '../../services/app-data.service';
 
 @Component({
   selector: 'app-car-maint',
@@ -6,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-maint.component.css']
 })
 export class CarMaintComponent implements OnInit {
+  CarList: Array<Car>;
 
-  constructor() { }
+  constructor(private router: Router, private appDataService: AppDataService) {
+    appDataService.getCars().subscribe((data) => {this.CarList = data; });
+   }
 
   ngOnInit() {
   }
