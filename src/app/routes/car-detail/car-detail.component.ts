d14dcae86f57bc41f772c0127f16dd50e8926653
Oleh Petraniuk <olehshort@gmail.com>
2019-01-11 08:49:41 +0100

    Created logical functionalityr of car-detail

--- src/app/routes/car-detail/car-detail.component.ts
+++ src/app/routes/car-detail/car-detail.component.ts
@@ -10,10 +10,48 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./car-detail.component.css']
 })
 export class CarDetailComponent implements OnInit {
-
-  constructor() { }
+  car: Car;
+  carDefinitionInput: Array<FieldInput> = [
+    {
+      key: 'id',
+      type: 'number',
+      isId: true,
+      label: 'Id',
+      required: true
+    },
+    {
+      key: 'name',
+      type: 'string',
+      isId: false,
+      label: 'Car name',
+      required: true
+    },
+    {
+      key: 'model',
+      type: 'string',
+      isId: false,
+      label: 'Car model',
+      required: true
+    },
+    {
+      key: 'price',
+      type: 'number',
+      isId: false,
+      label: 'Car price value',
+      required: true
+    }
+  ];
+  operation: string; // edit/ read/ create
+  errorMessage: string;
+  constructor(private router: Router, private route: ActivatedRoute, private appDataService: AppDataService) { }
 
   ngOnInit() {
+    this.operation = this.route.snapshot.params['operation'];
+    if( this.operation === 'create') {
+      this.car = { id: 0, name: '', model: '', price: null};
+    } else {
+      this.appDataService.getCar(+this.route.snapshot.params['id']).subscribe((car: Car) => this.car = car);
+    }
   }
 
 }
