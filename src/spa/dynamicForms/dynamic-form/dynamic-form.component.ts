9171bee0aa8ea85ae63982a112ea31658e63977a
Oleh Petraniuk <olehshort@gmail.com>
2019-01-12 08:29:22 +0100

    Added logical functionality to dynamic-form

--- src/spa/dynamicForms/dynamic-form/dynamic-form.component.ts
+++ src/spa/dynamicForms/dynamic-form/dynamic-form.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, OnChanges, SimpleChanges, Output } from '@angular/core';
+import {Location} from '@angular/common';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FieldInput } from '../field-interface';
+
 
 @Component({
   selector: 'spa-dynamic-form',
   templateUrl: './dynamic-form.component.html',
   styleUrls: ['./dynamic-form.component.css']
 })
-export class DynamicFormComponent implements OnInit {
+export class DynamicFormComponent implements OnInit, OnChanges {
+  @Input() vm: any;
+  @Input() vmDefinition: Array<FieldInput>;
+  @Input() operation: string;
+  @Input() errorMessage: string;
+  @Output() update: EventEmitter<any> = new EventEmitter();
+  @Output() create: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  form: FormGroup;
+  status: string;
+  submitted = false;
+  vmCopy: any;
+
+  constructor(
+    private route: ActivatedRoute, private router: Router,
+    private location: Location
+  ) { }
+
+  clearForm() {
+    const group ={};
+    this.vmCopy = Object.assign({}, this.vm);
+    this.vmDefinition.forEach(field => {
+      group[field.key] = field.required ?
+      new FormControl(this.vmCopy[field.key], Validators.required) :
+      new FormControl(this.vmCopy[field.key]);
+    });
+    this.form = new FormGroup(group);
+}
 
   ngOnInit() {
+    this.clearForm();
+    this.route.params.subscribe(params => {
+      this.operation = params['operation'];
+      this.clearForm();
+    });
+  } 
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes['errorMessage'].currentValue && this.status === 'waiting') {
+      this.status = '';
+    }
   }
 
 }
