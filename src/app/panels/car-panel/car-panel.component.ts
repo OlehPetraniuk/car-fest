0f10c72e7779325f03b5972890fa8dca23a44ce2
Oleh Petraniuk <olehshort@gmail.com>
2019-01-18 02:00:33 +0100

    Added visibility to car-panel

--- src/app/panels/car-panel/car-panel.component.ts
+++ src/app/panels/car-panel/car-panel.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Car } from '../../services/car-interface';
+import { visibility } from '../../../spa/services/animations';
 
 @Component({
   selector: 'app-car-panel',
   templateUrl: './car-panel.component.html',
-  styleUrls: ['./car-panel.component.css']
+  styleUrls: ['./car-panel.component.css'],
+  animations: [visibility]
 })
 export class CarPanelComponent implements OnInit {
   @Input() car: Car;
